ad5d48e6ff9fd2922fb3810e41712e7db175e514
MalcolmQ <55450029+MalcolmQ@users.noreply.github.com>
2026-02-16 02:12:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//show Side Navigation bar
+ function showSidenav() {
+      const sidenav = document.querySelector('.sidenav');
+      sidenav.style.display = 'flex';
+    }
+
+  function hideSidenav() {
+      const sidenav = document.querySelector('.sidenav');
+      sidenav.style.display = 'none';
+    }
